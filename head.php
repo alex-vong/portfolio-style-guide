876c623eeb7d22afc9cf3299a06643c6827dcd36
Alex Vong <avong003@gmail.com>
2021-10-21 22:34:12 -0700

    head and header

---
+++ head.php
@@ -0,0 +1,29 @@
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+	<title>Portfolio Style Guide</title>
+	<meta name='description' content='[[insert description]]'>
+	<meta property='og:image' content='images/default.png'>
+
+	<link rel='stylesheet' href='css/style.css'>
+</head>
+
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Portfolio Style Guide</title>
+		<meta
+			name="og:description"
+			content="Alex Vong Style-Guide"
+		/>
+	
+		<meta property="og:title" content="Portfolio Style-Guide" />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content=[[meta-image]]] />
+		<link rel="shortcut icon" type="image/jpg" href=[[favicon]] />
+
+		<link rel='stylesheet' href='css/style.css'>
+</head>
