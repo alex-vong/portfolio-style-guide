49c8a8e4b2634e77562fef9d71a105a962a1c37e
Alex Vong <avong003@gmail.com>
2021-10-21 22:34:21 -0700

    created footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+			<!-- STUFF -->
+		</main>
+
+
+		<footer class='site-footer'>
+		<inner-column>
+
+			<!-- no headers in footers allowed? -->
+			<div class='what-to-call-it-then'>
+				
+
+			</div>
+
+			<!-- <?php include('modules/site-map.php'); ?> -->
+
+		</inner-column>
+		</footer>
+
+	</body>
+</html>
