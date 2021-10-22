240462acc52d53577bf40b2a7cef643b41093dec
Alex Vong <avong003@gmail.com>
2021-10-21 22:35:43 -0700

    created colors php and css

---
+++ modules/colors.php
@@ -0,0 +1,13 @@
+<h2 class="section-heading attention-voice">Color Swatches / Ideas</h2>
+
+<div class="color-grid">
+	<div class="color calm-voice">#000</div>
+	<div class="color calm-voice">#fff</div>
+	<div class="color calm-voice">#8c57fe</div>
+	<div class="color calm-voice">#5b17ee</div>
+	<div class="color calm-voice">#281e3d</div>
+	<div class="color calm-voice">#b0a8b9</div>
+	<div class="color calm-voice">Gradient</div>
+	<div class="color calm-voice">Gradient background? </div>
+	<div class="color calm-voice">Gradient background 2? </div>
+</div>
