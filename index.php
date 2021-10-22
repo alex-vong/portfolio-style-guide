de462fa1863be528320046261481f4820cc8564d
Alex Vong <avong003@gmail.com>
2021-10-21 22:34:00 -0700

    index and css set up

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+<?php include('header.php'); ?>
+
+
+
+<section class='page-section type-face'>
+	<inner-column>
+	 	<?php include('modules/type-face.php'); ?>  
+
+	</inner-column>
+</section>
+
+<section class='page-section colors'>
+	<inner-column>
+	 	<?php include('modules/colors.php'); ?>  
+	</inner-column>
+</section>
+
+<section class='page-section site-map'>
+	<inner-column>
+	 	<?php include('modules/site-map.php'); ?>  
+	</inner-column>
+</section>
+
+<section class='page-section voices'>
+	<inner-column>
+	 	<?php include('modules/voices.php'); ?>  
+	</inner-column>
+</section>
+
+<?php include('footer.php'); ?>
