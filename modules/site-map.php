9c0ae88ac3932d6a4928528a3455d6296e4a9ed2
Alex Vong <avong003@gmail.com>
2021-10-21 22:36:45 -0700

    site-map and 404

---
+++ modules/site-map.php
@@ -0,0 +1,45 @@
+
+	<h2 class="section-heading attention-voice">Site Map</h2>
+
+	<nav>
+
+		<div class="logo">
+			<h1 class="loud-voice">AOL</h1>
+		</div>
+		<ul>
+			<li><a href="#" class='calm-voice'>About</a></li>
+			<li><a href="#" class='calm-voice'>Projects</a></li>
+			<li><a href="#" class='calm-voice'>Contact</a></li>
+			<li><a href="#" class='calm-voice'>Resume</a></li>
+		</ul>
+		
+	</nav>
+
+
+
+
+<h2 class="section-heading attention-voice">Bread crumbs</h2>
+<div class="bread-crumbs">
+	<div class="bc-light bc">
+			<ul>
+				<li><a href="#" class='calm-voice'>AOL Home</a></li>
+				<div> > </div>
+				<li><a href="#" class='calm-voice'>Projects</a></li>
+				<div> > </div>
+				<li><a href="#" class='calm-voice'>Pokedex</a></li>
+			</ul>
+	</div>
+
+	<div class="bc-dark bc">
+			<ul>
+				<li><a href="#" class='calm-voice'>AOL Home</a></li>
+				<div> / </div>
+				<li><a href="#" class='calm-voice'>Blog</a></li>
+				<div> / </div>
+				<li><a href="#" class='calm-voice'>Post</a></li>
+			</ul>
+	</div>
+</div>
+
+
+
