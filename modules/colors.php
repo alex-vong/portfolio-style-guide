dac64191a70cef6cffc323e9be3b5aff80ddbe32
Alex Vong <avong003@gmail.com>
2021-10-24 13:08:41 -0700

    changed colors to rgb

--- modules/colors.php
+++ modules/colors.php
@@ -1,13 +1,12 @@
 <h2 class="section-heading attention-voice">Color Swatches / Ideas</h2>
 
 <div class="color-grid">
-	<div class="color calm-voice">#000</div>
-	<div class="color calm-voice">#fff</div>
-	<div class="color calm-voice">#8c57fe</div>
-	<div class="color calm-voice">#5b17ee</div>
-	<div class="color calm-voice">#281e3d</div>
-	<div class="color calm-voice">#b0a8b9</div>
-	<div class="color calm-voice">Gradient</div>
-	<div class="color calm-voice">Gradient background? </div>
-	<div class="color calm-voice">Gradient background 2? </div>
+	<div class="color calm-voice">$white #fff</div>
+	<div class="color calm-voice">$dark #281e3d</div>
+	<div class="color calm-voice">$primary #8c57fe</div>
+	<div class="color calm-voice">$secondary #5b17ee</div>
+	<div class="color calm-voice">$light #b0a8b9</div>
+	<div class="color calm-voice">$gradient Gradient</div>
+
+
 </div>
