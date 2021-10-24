007c6bbd56216859479bd97cb7efbfb21475774e
Alex Vong <avong003@gmail.com>
2021-10-24 13:08:57 -0700

    added contact grid and buttons

---
+++ modules/buttons.php
@@ -0,0 +1,21 @@
+<h2 class="section-heading attention-voice">Buttons</h2>
+
+<div class="buttons-list">
+	<ul>
+			<li>
+				<a href="#" class='btn btn-secondary'>My Projects</a>
+			</li>
+
+			<li>
+				<a href="#" class='btn btn-gradient'>Get In Touch</a>
+			</li>
+
+			<li>
+				<a href="#" class='btn btn-light'>My Writing</a>
+			</li>
+
+
+
+	</ul>
+</div>
+
---
+++ modules/contact.php
@@ -0,0 +1,18 @@
+<h2 class="section-heading attention-voice">Contact</h2>
+
+<div class="contact-me">
+
+	<div class="get-in-touch">
+		<h3 class="attention-voice">Get In Touch</h3>
+	</div>
+
+	<div class="contact-grid">
+		<div><a href="#" class="calm-voice">Email</a></div>
+		<div><a href="#" class="calm-voice">Instagram</a></div>
+		<div><a href="#" class="calm-voice">Twitter</a></div>
+		<div><a href="#" class="calm-voice">Facebook</a></div>
+		<div><a href="#" class="calm-voice">LinkedIn</a></div>
+		<div><a href="#" class="calm-voice">Substack</a></div>
+
+	</div>
+</div>
