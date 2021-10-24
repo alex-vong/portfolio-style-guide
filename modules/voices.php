9daba9fc46fe33048304c617d650a57b6dbbd495
Alex Vong <avong003@gmail.com>
2021-10-24 13:07:28 -0700

    updated voices classes

--- modules/voices.php
+++ modules/voices.php
@@ -2,9 +2,9 @@
   <h2 class="section-heading attention-voice">Types of Voices</h2>
 
   <div class="voice-types">
-  	<div class="whisper-voice">This is a whisper voice that I use when I am in the library.</div>
-  	<div class="normal-voice">This is a normal voice I use when I am talking to another person.</div>
-  	<div class="stern-voice">This is the stern voice I use when I scold my doggo for being a bad boy.</div>
-  	<div class="shout-voice">This is the shouting voice I use when cheering my favorite sports team.</div>
-  	 <div class="maniac-voice">This is the shreik voice I make when I am running for my life away from a bear</div>
+  	<div class="quiet-voice">This is a whisper voice that I use when I am in the library.</div>
+  	<div class="calm-voice">This is a normal voice I use when I am talking to another person.</div>
+  	<div class="attention-voice">This is the stern voice I use when I scold my doggo for being a bad boy.</div>
+  	<div class="intro-voice">This is the screaming voice I use when cheering my favorite sports team.</div>
+  	 <div class="loud-voice">This is the shreik voice I make when I am running for my life away from a bear</div>
   </div>
